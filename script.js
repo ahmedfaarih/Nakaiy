359cf6a4588dbba34fdd8b46e25322e222461c70
faarihOffline <faarih@office>
2021-09-09 16:50:01 +0500

    JavaScript code for varibales

---
+++ script.js
@@ -0,0 +1,5 @@
+const mainCricle = document.getElementById('circle-orange');
+let today = new Date();
+let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+console.log(date);
